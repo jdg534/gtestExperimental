2b026092d268bfff71430f7d31ba106b740e1e1b
Josh <joshdgibson21@outlook.com>
2016-12-30 21:24:17 +0000

    Added VS 2013 build script.

---
+++ BuildVs2013Solution.bat
@@ -0,0 +1,13 @@
+REM the .gitignore ignores a folder called "Build" (case sensitive)
+
+
+
+mkdir Build
+
+cd Build
+
+cmake ../ -G "Visual Studio 12 2013"
+
+ECHO The Build solution script has finshed
+
+PAUSE
